80edbd7a62ae6f1074de2bbdbf1a951f9fc48f45
Orlfi <orlfi@mail.ru>
2021-11-01 17:10:15 +0300

    add task 1,2

---
+++ 6.Oop/Lesson-3/Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceString = "Пример текстовой строки";
+
+            Console.WriteLine($"Исходная строка: {sourceString}");
+            Console.WriteLine($"Перевернутая строка: {sourceString.Reverse()}");
+            Console.ReadKey();
+        }
+    }
+}
