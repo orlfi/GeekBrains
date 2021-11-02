adafebf4e4035f3d183b5d4afde5bfdd004077df
Orlfi <orlfi@mail.ru>
2021-11-02 17:20:10 +0300

    add homework 4 task 1

---
+++ 6.Oop/Lesson-4/Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new();
+            building.SetHeight(30);
+            building.SetFloorCount(9);
+            building.SetFlatCount(72);
+            building.SetEntranceCount(2);
+            PrintBuildingInfo(building);
+
+            Building building2 = new();
+            building2.SetHeight(50);
+            building2.SetFloorCount(16);
+            building2.SetFlatCount(320);
+            building2.SetEntranceCount(4);
+            Console.WriteLine();
+            PrintBuildingInfo(building2);
+        }
+
+        public static void PrintBuildingInfo(Building building)
+        {
+            Console.WriteLine($"Параметры здания №{building.GetNumber()}:");
+            Console.WriteLine($"Высота этажа: {building.GetFloorHeight():0.##}");
+            Console.WriteLine($"Количество квартир в подъезде: {building.GetFlatCountInEntrance()}");
+            Console.WriteLine($"количества квартир на этаже: {building.GetFlatCountOnFloor()}");
+
+        }
+    }
+}
