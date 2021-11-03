4a96b48fb6a5e9f868adf124adf0d91344eb0ee9
Orlfi <orlfi@mail.ru>
2021-11-03 21:27:44 +0300

    add task 2

---
+++ 6.Oop/Lesson-4/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int buildingNumber1 = Creator.CreateBuilding(30, 72, 9, 2);
+            Console.WriteLine(Creator.GetBuildingByNumber(buildingNumber1).ToString());
+
+            int buildingNumber2 = Creator.CreateBuilding(50, 320, 16, 4);
+            Console.WriteLine(Creator.GetBuildingByNumber(buildingNumber2).ToString());
+
+            Creator.DeleteBuilding(buildingNumber2);
+            Console.ReadKey();
+        }
+    }
+}
