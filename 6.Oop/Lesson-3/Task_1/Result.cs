c6db9ddf9cfae96b0ca63f634e53f7d51573b59e
Федор Orlfi <orlfi@mail.ru>
2021-11-21 15:26:26 +0300

    переделал Result на readonly struct

--- 6.Oop/Lesson-3/Task_1/Result.cs
+++ 6.Oop/Lesson-3/Task_1/Result.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 
 namespace Task_1
 {
-    public ref readonly struct Result<T>
+    public readonly ref struct Result<T>
     {
         public bool Success => Error is null;
 
-        public T Data { get; set; }
+        public T Data { get; }
 
-        public Exception Error { get; set; }
+        public Exception Error { get; }
 
-        public static implicit operator Result<T>(T value) => new Result<T>() { Data = value };
+        public static implicit operator Result<T>(T value) => new Result<T>(value);
 
-        public static implicit operator Result<T>(Exception exception) => new Result<T>() { Error = exception };
+        public static implicit operator Result<T>(Exception exception) => new Result<T>(exception);
 
-        public static implicit operator Result<T>((T value, Exception exception) t) => new Result<T>() { Data = t.value, Error = t.exception };
+        public static implicit operator Result<T>((T value, Exception exception) tuple) => new Result<T>(tuple.value, tuple.exception);
 
         public static implicit operator T(Result<T> result) => result.Data;
 
         public static implicit operator Exception(Result<T> result) => result.Error;
+
+        public Result(T data) : this(data, null) { }
+
+        public Result(Exception error) : this(default(T), error) { }
+
+        public Result(T data, Exception error) => (Data, Error) = (data, error);
     }
 }
