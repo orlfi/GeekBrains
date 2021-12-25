ce6a6045a8672eca1f0af50c864d4fa7a3243b5c
orlfi <orlfi@mail.ru>
2021-12-25 18:49:59 +0300

    Добавить доменную модель

---
+++ 7.AspnetMvc/Lesson-8/Hardware/Hardware.Domain/Hardware.cs
@@ -0,0 +1,16 @@
+﻿using Hardware.Domain.Base;
+
+namespace Hardware.Domain;
+
+public class Hardware : NamedEntity
+{
+    public string Description { get; set; }
+
+    public decimal  Cost { get; set; }
+
+    public DateTime InstallationDate { get; set; }
+
+    public HardwareType HardwareType { get; set; }
+
+    public Office Office { get; set; }
+}
