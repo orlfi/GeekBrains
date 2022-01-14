5d50b706e6891d18eabaef6eaeffc3a2ba4b29e9
Orlfi <orlfi@mail.ru>
2022-01-14 16:14:34 +0300

    Добавил доменную модель_

---
+++ 8.Secure/Lesson-1/BankCards.Domain/Card.cs
@@ -0,0 +1,37 @@
+namespace BankCards.Domain;
+
+public class Card
+{
+    /// <summary>
+    /// Номер карты
+    /// </summary>
+    /// <value>16 цифр</value>
+    public string Number { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Тип карты
+    /// </summary>
+    public CardTypes Type { get; set; }
+
+    /// <summary>
+    /// Дата изготовления
+    /// </summary>
+    public DateTime Created { get; set; }
+
+    /// <summary>
+    /// Срок действия по
+    /// </summary>
+    public DateTime ValidThru { get; set; }
+
+    /// <summary>
+    /// Владелец
+    /// </summary>
+    /// <value>Только латинские буквы</value>
+    public string Owner { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Защитный код
+    /// </summary>
+    /// <value>3 цифры</value>
+    public int Cvc { get; set; }
+}
