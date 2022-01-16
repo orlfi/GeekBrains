c8516a0b4e044e4710900cef8702cfe3e08a7085
orlfi <orlfi@mail.ru>
2022-01-16 15:54:37 +0300

    Добавил мапперы вручную

---
+++ 8.Secure/Lesson-1/BankCards.ApiOrm/Mappers/CardMappers.cs
@@ -0,0 +1,50 @@
+﻿using BankCards.ApiOrm.DTO.Cards;
+using BankCards.Domain;
+
+namespace BankCards.ApiOrm.Mappers
+{
+    public static class CardMappers
+    {
+        public static Card ToCard(this CardCreateRequest request) =>
+            new Card()
+            {
+                Number = request.Number,
+                Type = (CardTypes)request.Type,
+                Created = request.Created,
+                ValidThru = request.ValidThru,
+                Cvc = request.Cvc,
+                Owner = request.Owner,
+            };
+
+        public static Card ToCard(this CardUpdateRequest request) =>
+            new Card()
+            {
+                Id = request.Id,
+                Number = request.Number,
+                Type = (CardTypes)request.Type,
+                Created = request.Created,
+                ValidThru = request.ValidThru,
+                Cvc = request.Cvc,
+                Owner = request.Owner,
+            };
+
+        public static CardResponse? ToResponse(this Card? entity) =>
+            entity is null ? null : 
+            new CardResponse()
+            {
+                Id = entity.Id,
+                Number = entity.Number,
+                Type = (int)entity.Type,
+                Created = entity.Created,
+                ValidThru = entity.ValidThru,
+                Cvc = entity.Cvc,
+                Owner = entity.Owner,
+            };
+
+        public static CardsResponse ToResponse(this IEnumerable<Card> entity) =>
+            new CardsResponse()
+            {
+                Cards = entity.Select(x => x.ToResponse()),
+            };
+    }
+}
