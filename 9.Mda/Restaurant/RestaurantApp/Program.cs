feacd80b5d803bf1c28160c692e8c3429d97aa69
orlfi <orlfi@mail.ru>
2022-02-23 15:31:00 +0300

    Создал инфраструктуру приложения

---
+++ 9.Mda/Restaurant/RestaurantApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using RestaurantApp;
+
+static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(ConfigureApp)
+    .ConfigureServices(ConfigureServices)
+    .UseSerilog(ConfigureLogger);
+
+static void ConfigureApp(HostBuilderContext context, IConfigurationBuilder builder)
+{
+}
+
+static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+{
+    services.AddSingleton<Application>();
+}
+
+static void ConfigureLogger(HostBuilderContext context, LoggerConfiguration config)
+{
+    config.ReadFrom.Configuration(context.Configuration);
+}
+
+using IHost host = CreateHostBuilder(args).Build();
+await host.Services.GetRequiredService<Application>().RunAsync();
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
