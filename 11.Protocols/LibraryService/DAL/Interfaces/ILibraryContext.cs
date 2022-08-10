7ad4a4302a7573d936ca1954b6cf1df9709f6e3e
Orlfi <orlfi@mail.ru>
2022-08-10 11:18:56 +0300

    add asmx service

---
+++ 11.Protocols/LibraryService/DAL/Interfaces/ILibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService.DAL.Interfaces
+{
+    public interface ILibraryContext
+    {
+        IList<Book> Books { get; }
+    }
+}
---
+++ 11.Protocols/LibraryService/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace LibraryService.Models
+{
+
+    public class Book
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Lang { get; set; }
+
+        public int Pages { get; set; }
+
+        public int AgeLimit { get; set; }
+
+        public Author[] Authors { get; set; }
+
+        public string PublicationDate { get; set; }
+    }
+}
