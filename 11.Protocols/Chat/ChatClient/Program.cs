e5c30338c7215db92bf0a9cb9e72f8578663ea47
orlfi <orlfi@mail.ru>
2022-08-21 23:50:38 +0300

    Homework 5

---
+++ 11.Protocols/Chat/ChatClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using ChatClient;
+
+Console.Write("Login: ");
+var login = Console.ReadLine();
+
+await using var client = new Chat(" http://localhost:5208/chat", login);
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($"{login}>");
+    var message = Console.ReadLine();
+    await client.SendAsync(message);
+}
---
+++ 11.Protocols/Chat/ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Data;
+using Microsoft.AspNetCore.SignalR;
+using System;
+
+namespace ChatServer.Hubs
+{
+    public class ChatHub: Hub
+    {
+        private readonly ILogger<ChatHub> _logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"Установлено новое соединение {Context.ConnectionId}");
+            _logger.LogInformation($"Установлено новое соединение {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.All.SendAsync("Notify", $"Закрыто соединение {Context.ConnectionId}");
+            _logger.LogInformation($"Закрыто соединение {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(Message message)
+        {
+            await Clients.Others.SendAsync("RecieveMessage", message);
+        }
+    }
+}
